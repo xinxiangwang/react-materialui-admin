6d584a38ce7bfaa6f474ffaae2e5782058daf96a
wxx <myf4641@dingtalk.com>
2021-04-30 18:02:46 +0800

    del console

--- src/pages/nested/menu1/menu1-1/index.tsx
+++ src/pages/nested/menu1/menu1-1/index.tsx
@@ -1,12 +1,6 @@
 import React, { useEffect } from 'react'
 
 export default function Menu1_1() {
-  useEffect(() => {
-    console.log('被渲染menu1')
-    return () => {
-      console.log('被渲染menu1asdasdasd')
-    }
-  })
   return (
     <div>
       <h1>menu1-1</h1>
