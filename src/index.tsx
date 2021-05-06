db9ea3ad13b8c7200ad7fd9e927db4b6fa5649ca
wxx <myf4641@dingtalk.com>
2021-05-06 18:24:46 +0800

    add normalize.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
-import 'react-app-polyfill/ie11'
-import 'react-app-polyfill/stable'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css'
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
